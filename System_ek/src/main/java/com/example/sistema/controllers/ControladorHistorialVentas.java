3c122d1098f842110630df8308807253f9eefc8e
EDUARDO2202A <eduardocanche.mcc@gmail.com>
2025-11-10 20:24:00 -0600

    ya no hay error

--- System_ek/src/main/java/com/example/sistema/controllers/ControladorHistorialVentas.java
+++ System_ek/src/main/java/com/example/sistema/controllers/ControladorHistorialVentas.java
@@ -10,7 +10,7 @@ import javafx.scene.control.TextArea;
 //hola juan calos
 
 public class ControladorHistorialVentas {
-
+// en los privates debes de cmabiar lo que esta dentro de los ¿? por los modelos corresponientes
     @FXML
     private Button regresarButton;
 
@@ -38,10 +38,28 @@ public class ControladorHistorialVentas {
 
     @FXML
     public void initialize() {
-
+        // Lógica de tu compañero:
+        // 1. Configurar las columnas (setCellValueFactory).
+        // 2. Cargar los datos de las ventas en 'ventasTable'.
+        // 3. Añadir listener a 'ventasTable' para mostrar detalles en 'detallesVentaArea'.
 
         System.out.println("ControladorHistorialVentas inicializado.");
     }
 
+    @FXML
+    void manejarRegreso(ActionEvent event) {
+        //para regresar a la vista principal
 
+        System.out.println("Botón 'Regresar' presionado.");
+    }
+
+
+    @FXML
+    void reimprimirTicket(ActionEvent event) {
+        // con este deberias reinprimir el ticket
+        //  debes de obtener la venta seleccionada de 'ventasTable'.
+
+
+        System.out.println("Botón 'Reimprimir Ticket' presionado.");
+    }
 }
