06dfc7f2ce5d7b71a18a5f1db47dbec0f44dd02f
EDUARDO2202A <eduardocanche.mcc@gmail.com>
2025-11-10 19:43:26 -0600

    hola juancalos

--- System_ek/src/main/java/com/example/sistema/controllers/ControladorHistorialVentas.java
+++ System_ek/src/main/java/com/example/sistema/controllers/ControladorHistorialVentas.java
@@ -7,7 +7,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 
-
+//hola juan calos
 
 public class ControladorHistorialVentas {
 
